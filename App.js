88943d18aa24a7f37da5750f1ff298ed9dde07a0
Simon Márton <simon.marton@bitrise.io>
2021-10-21 22:57:15 +0200

    test commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello there!</Text>
+      <Text>Hello there, bahah</Text>
       <StatusBar style="auto" />
     </View>
   );
